a3fea4304c299d473005de2ac525ef9466f01b36
Chris <chris.g.meier@gmail.com>
2016-06-24 19:49:50 -0400

    Added ability to initialize TrueTime API

--- patstop.py
+++ patstop.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from pghbustime import *
+
+my_key = "your_api_key"
+api = BustimeAPI(my_key) #default is to return JSON
 
 app = Flask(__name__)
 
@@ -14,3 +18,5 @@ def get_stop_info(stop_id):
 
 if __name__ == "__main__":
     app.run()
+
+
