94a921e1a1c1dc45ed7f9956cd24964292845efe
Chris <chris.g.meier@gmail.com>
2016-06-24 20:32:51 -0400

    Added ability to look up next bus at a stop and the next bus of a specific route at a stop

--- patstop.py
+++ patstop.py
@@ -2,6 +2,7 @@ from flask import Flask
 from pghbustime import *
 
 my_key = "your_api_key"
+
 api = BustimeAPI(my_key) #default is to return JSON
 
 app = Flask(__name__)
@@ -12,11 +13,29 @@ def home_base():
 
 @app.route('/stops/<stop_id>', methods=['GET'])
 def get_stop_info(stop_id):
+    
     return 'This is the info for stop %s' % stop_id
 
+#Get the next bus arriving at your stop
+@app.route('/stops/<stop_id>/next', methods=['GET'])
+def get_next_bus(stop_id):
+    current_stop = Stop.get(api, stop_id)
+    next_buses = current_stop.predictions()
+    next_bus = next_buses.next()
 
+    return 'The next bus is %s arriving at %s' % (next_bus.route, next_bus.eta)
+
+#Get the next specified bus arriving at your stop
+@app.route('/stops/<stop_id>/<route>', methods=['GET'])
+def get_next_route(stop_id, route):
+    current_stop = Stop.get(api, stop_id)
+    next_route = current_stop.predictions(route=route).next()
+
+    #Need to catch 'No Service'
+
+    return 'The next %s is arriving at %s' % (route, next_route.eta)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 
